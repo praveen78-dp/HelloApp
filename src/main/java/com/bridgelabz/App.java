a5e625d878589bfbd7f07426f733db7816109e3a
Praveen <pm9693@srmist.edu.in>
2026-03-31 21:38:58 +0530

    feat: implement UC1 - display hello world

---
+++ src/main/java/com/bridgelabz/App.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class App {
+    public static void main(String[] args) {
+        // UC1: Basic output to console
+        System.out.println("Hello World");
+    }
+}
