501534408e6fd56231abe32c74fa2a397d158b10
Praveen <pm9693@srmist.edu.in>
2026-03-31 23:21:43 +0530

    feat: implement UC7 - join arguments using String.join()

--- src/main/java/com/bridgelabz/App.java
+++ src/main/java/com/bridgelabz/App.java
@@ -1,23 +1,16 @@
 package com.bridgelabz;
 
 /**
- * UC6: Display "Hello" with Multiple Arguments and remove Trailing Delimiter
+ * UC7: Display "Hello" using String.join() for multiple arguments
  */
 public class App {
     public static void main(String[] args) {
         if (args.length > 0) {
-            String greeting = "Hello ";
+            // String.join takes the delimiter (", ") and the array (args)
+            // It automatically places the comma ONLY between elements
+            String joinedNames = String.join(", ", args);
             
-            // Build the string with commas
-            for (String name : args) {
-                greeting += name + ", ";
-            }
-
-            // greeting is currently "Hello Praveen, Rahul, "
-            // We need to remove the last ", " (length - 2)
-            greeting = greeting.substring(0, greeting.length() - 2);
-            
-            System.out.println(greeting + "!");
+            System.out.println("Hello " + joinedNames + "!");
         } else {
             System.out.println("Hello World!");
         }
